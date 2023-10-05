e6ed618315c89d73ecb26c55d5195ff73e2a43e1
I-inoluwa <ajileyeolaoluwa@yahoo.com>
2023-10-05 23:26:16 +0100

    Handled movements, i.e., navigation through the maze

---
+++ Search/task1/7-movements.c
@@ -0,0 +1,113 @@
+#include "main.h"
+
+state move_template(state cur_state)
+{
+	state new_state = NULL;
+
+	if (cur_state == NULL)
+		return (NULL);
+
+	new_state = malloc(sizeof(position));
+	if (new_state == NULL)
+		return (NULL);
+
+	new_state->coord = int_copy(cur_state->coord, 2);
+
+	return (new_state);
+}
+
+state up(state cur_state, char **arr)
+{
+	int x = 0, y = 0;
+	state new_state = NULL;
+
+	new_state = copy_state(cur_state);
+	if (new_state == NULL)
+		return (NULL);
+
+	x = (cur_state->coord)[0], y = (cur_state->coord)[1];
+
+	if (x == 0)
+	{
+		free_state(new_state);
+		return (NULL);
+	}
+
+	(new_state->coord)[0] = x - 1;
+	new_state->c = arr[x - 1][y];
+	new_state->parent = cur_state;
+
+	return (new_state);
+}
+
+state down(state cur_state, char **arr)
+{
+	int x = 0, y = 0;
+	state new_state = NULL;
+
+	new_state = copy_state(cur_state);
+	if (new_state == NULL)
+		return (NULL);
+
+	x = (cur_state->coord)[0], y = (cur_state->coord)[1];
+
+	if (arr[x + 1] == NULL)
+	{
+		free_state(new_state);
+		return (NULL);
+	}
+
+	(new_state->coord)[0] = x + 1;
+	new_state->c = arr[x + 1][y];
+	new_state->parent = cur_state;
+
+	return (new_state);
+}
+
+state left(state cur_state, char **arr)
+{
+	int x = 0, y = 0;
+	state new_state = NULL;
+
+	new_state = copy_state(cur_state);
+	if (new_state == NULL)
+		return (NULL);
+
+	x = (cur_state->coord)[0], y = (cur_state->coord)[1];
+
+	if (y == 0)
+	{
+		free_state(new_state);
+		return (NULL);
+	}
+
+	(new_state->coord)[1] = y - 1;
+	new_state->c = arr[x][y - 1];
+	new_state->parent = cur_state;
+
+	return (new_state);
+}
+
+state right(state cur_state, char **arr)
+{
+	int x = 0, y = 0;
+	state new_state = NULL;
+
+	new_state = copy_state(cur_state);
+	if (new_state == NULL)
+		return (NULL);
+
+	x = (cur_state->coord)[0], y = (cur_state->coord)[1];
+
+	if (arr[x][y + 1] == '\0')
+	{
+		free_state(new_state);
+		return (NULL);
+	}
+
+	(new_state->coord)[1] = y + 1;
+	new_state->c = arr[x][y + 1];
+	new_state->parent = cur_state;
+
+	return (new_state);
+}
